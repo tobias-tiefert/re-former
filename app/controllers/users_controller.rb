2478ea5b9c14e437c1660fef2cdc42bad876d0ea
Tobias Tiefert <244593488+tobias-tiefert@users.noreply.github.com>
2026-04-17 12:12:55 +0200

    Add allowed_user_params private method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(allowed_user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def allowed_user_params
+    params.expect(user: [ :username, :email, :password ])
   end
 end
